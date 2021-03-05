e3d210521c79e1fb8d7ae9e46aa64797f7372459
unknown <dobiecarly@gmail.com>
2021-03-04 18:26:08 -0600

    added work experience positions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { HashRouter, Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage.js';
 // import AboutMe from './pages/aboutme.js';
-// import WorkExperience from './pages/workexperience.js';
+import WorkExperience from './pages/workexperience.js';
 // import CurrentProjects from './pages/currentprojects.js';
 // import ContactInfo from './pages/contactinfo.js';
 import ComingSoon from './pages/comingsoon.js';
@@ -17,7 +17,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/aboutme" component={ComingSoon} />
-            <Route exact path="/workexperience" component={ComingSoon} />
+            <Route exact path="/workexperience" component={WorkExperience} />
             <Route exact path="/currentprojects" component={ComingSoon} />
             <Route exact path="/contactinfo" component={ComingSoon} />
             <Route component={NotFound} />
--- src/pages/workexperience.js
+++ src/pages/workexperience.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(({
     textbox : {
-        paddingTop: "10vh",
-        margin: "2vw",
+        margin: "2vh",
     },
-    text : {
-        fontSize: "6vw",
+    heading : {
+        fontSize: "30px",
         fontFamily: "arial",
         color: "#BEBEBE"
+    },
+    position : {
+        textAlign: "left",
+        paddingLeft: "10vw",
+        paddingTop: "2vh",
+        fontSize: "25px",
+        fontFamily: "arial",
+        color: "#717171",
+    },
+    dates : {
+        fontStyle: "italic",
+        fontSize: "20px",
+        color: "#A1A1A1",
     }
 }))
 
@@ -19,8 +31,40 @@ export default function WorkExperience() {
     return (
         <body>
             <Box className={classes.textbox}>
-                <Typography className={classes.text}>
+                <Typography className={classes.heading}>
                         Work Experience
+                </Typography>
+            </Box>
+            <Box>
+                <Typography className={classes.position}>
+                    Full Stack Software Engineer Intern
+                    <Typography className={classes.dates}>
+                        Discover Financial Sevices
+                    </Typography>
+                </Typography>
+            </Box>
+            <Box>
+                <Typography className={classes.position}>
+                    Endpoint Device Technician
+                    <Typography className={classes.dates}>
+                        Northern Illinois University
+                    </Typography>
+                </Typography>
+            </Box>
+            <Box>
+                <Typography className={classes.position}>
+                    Summer IT Help
+                    <Typography className={classes.dates}>
+                        School District U-46
+                    </Typography>
+                </Typography>
+            </Box>
+            <Box>
+                <Typography className={classes.position}>
+                    IT Helpdesk Technician
+                    <Typography className={classes.dates}>
+                        IT Lighthouse
+                    </Typography>
                 </Typography>
             </Box>
         </body>
